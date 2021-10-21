7ed095bef78a361286c9149886948126893f6f2b
watasho358 <t8mmmxc11y@yahoo.co.jp>
2021-10-21 20:28:56 +0900

    ブログのタイトル一覧表示

--- tests/Feature/Controllers/BlogViewControllerTest.php
+++ tests/Feature/Controllers/BlogViewControllerTest.php
@@ -2,16 +2,27 @@
 
 namespace Tests\Feature\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class BlogViewControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test Index */
     function ブログのトップページを開ける()
     {
+        // ブログを3件登録
+        $blog1 = Blog::factory()->create();
+        $blog2 = Blog::factory()->create();
+        $blog3 = Blog::factory()->create();
+
         $this->get('/')
-             ->assertOK();
+             ->assertOK()
+             ->assertSee($blog1->title)
+             ->assertSee($blog2->title)
+             ->assertSee($blog3->title);
     }
 }
