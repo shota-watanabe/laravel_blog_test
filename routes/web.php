e3b864b9dccc70c0b50267e6c740329ce30d98ce
watasho358 <t8mmmxc11y@yahoo.co.jp>
2021-10-31 21:27:51 +0900

    ユーザー登録、その1

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Tests\Feature\Controllers\BlogViewControllerTest;
 use App\Http\Controllers\BlogViewController;
+use App\Http\Controllers\SignUpController;
 
 Route::get('/', [BlogViewController::class, 'index']);
 Route::get('blogs/{blog}', [BlogViewController::class, 'show']);
+Route::get('signup', [SignUpController::class, 'index']);
+Route::post('signup', [SignUpController::class, 'store']);
